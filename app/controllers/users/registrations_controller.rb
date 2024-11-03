95dc031503db2fc83bf44df4c7dfe625d31dee2d
Szymon Woźny <szwoz2001@gmail.com>
2024-11-03 13:50:28 +0100

    fixes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,18 +7,27 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def respond_with(resource, _opts = {})
     register_success && return if resource.persisted?
 
-    register_failed
+    register_failed(resource)
   end
 
   def register_success
     render json: { message: 'Signed up successfully.' }
   end
 
-  def register_failed
-    render json: { message: 'Something went wrong.' }
+  def register_failed(resource)
+    render json: { message: 'Something went wrong.', errors: resource.errors.full_messages },
+           status: :unprocessable_entity
   end
 
   def set_flash_message!(*)
     # Override to disable flash messages
+  end
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :username, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :username)
   end
 end
